181afb4a0514631e56d664e43e1332421948f177
Meshack Munyiri <meshmunyiriks@gmail.com>
2023-07-17 07:43:49 -0700

    c25

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints with printf
+ * Return: 0 (done)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (o);
+}
